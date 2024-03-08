7cdf881f0d6fc63d993e363c0c410ed13e112136
RealFars <109608714+RealFars@users.noreply.github.com>
2024-03-08 23:31:12 +0100

    the pwa should now reload on restart

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js")
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === "image",
     new workbox.strategies.NetworkFirst()
-    
-)
+);
